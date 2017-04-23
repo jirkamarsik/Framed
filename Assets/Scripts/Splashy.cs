5d0fdc1073e0628835eb01c35193a6139b0153c3
Jirka Marsik <jiri.marsik89@gmail.com>
2017-04-23 23:56:59 +0200

    Splashing everywhere with InkPainter

---
+++ Assets/Scripts/Splashy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Es.InkPainter;
+
+public class Splashy : MonoBehaviour {
+
+    public Brush splashBrush;
+    public float splashScale = 0.2f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var canvas = collision.gameObject.GetComponent<InkCanvas>();
+
+        if (canvas != null)
+        {
+            splashBrush.Scale = splashScale * this.GetComponent<Collider>().bounds.size.x;
+            canvas.Paint(splashBrush, collision.contacts[0].point);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
