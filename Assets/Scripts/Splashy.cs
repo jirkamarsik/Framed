79aee9019fb7f6c6786e72b43a0eff9976043f70
Jirka Marsik <jiri.marsik89@gmail.com>
2017-04-24 12:04:02 +0200

    Splashes are now independent of the scale of the splash surface

--- Assets/Scripts/Splashy.cs
+++ Assets/Scripts/Splashy.cs
@@ -6,7 +6,7 @@ using Es.InkPainter;
 public class Splashy : MonoBehaviour {
 
     public Brush splashBrush;
-    public float splashScale = 0.2f;
+    public float splashScale = 3.0f;
 
     void OnCollisionEnter(Collision collision)
     {
@@ -14,7 +14,19 @@ public class Splashy : MonoBehaviour {
 
         if (canvas != null)
         {
-            splashBrush.Scale = splashScale * this.GetComponent<Collider>().bounds.size.x;
+            Vector3 ballDimensions = this.GetComponent<Collider>().bounds.size;
+            float ballSize = System.Math.Max(ballDimensions.x, System.Math.Max(ballDimensions.y, ballDimensions.z));
+
+            // Assuming that the object we hit is the standard Unity Plane
+            float wallSizeX = 10 * collision.transform.localScale.x;
+            float wallSizeZ = 10 * collision.transform.localScale.z;
+
+            float scaleX = ballSize / wallSizeX;
+            float scaleZ = ballSize / wallSizeZ;
+
+            splashBrush.ScaleU = splashScale * scaleX;
+            splashBrush.ScaleV = splashScale * scaleZ;
+
             canvas.Paint(splashBrush, collision.contacts[0].point);
         }
 
