8850671c4e6e3f04d98d4c63e235c911fe5f8134
Jirka Marsik <jiri.marsik89@gmail.com>
2017-04-24 15:12:26 +0200

    Seamless painting across floor tiles

--- Assets/Scripts/Splashy.cs
+++ Assets/Scripts/Splashy.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Es.InkPainter;
 
-public class Splashy : MonoBehaviour {
-
+public class Splashy : MonoBehaviour
+{
     public Brush splashBrush;
     public float splashMinScale = 4.0f;
     public float splashMaxScale = 12.0f;
@@ -15,53 +15,67 @@ public class Splashy : MonoBehaviour {
     public float dropletMinReflection = 1.0f;
     public float dropletMaxReflection = 3.0f;
     public float dropletScatter = 3.0f;
-    
+
     float BallSize
     {
-        get {
+        get
+        {
             Vector3 ballDimensions = this.GetComponent<Collider>().bounds.size;
             return System.Math.Max(ballDimensions.x, System.Math.Max(ballDimensions.y, ballDimensions.z));
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        Stamp(collision.gameObject, collision.contacts[0].point);
-
-        int droplets = Mathf.FloorToInt(BallSize * Random.Range(dropletMinAmount, dropletMaxAmount));
-        for (int i = 0; i < droplets; i++)
+        if (other.gameObject.CompareTag("PaintTrigger"))
         {
-            GameObject droplet = Instantiate(this.gameObject);
-            droplet.transform.localScale = this.transform.localScale * Random.Range(dropletMinSize, dropletMaxSize);
-            Vector3 dropletVelocity = this.GetComponent<Rigidbody>().velocity; // preserve
-            dropletVelocity += Random.Range(dropletMinReflection, dropletMaxReflection) * collision.contacts[0].normal; // reflect
-            dropletVelocity += dropletScatter * Random.insideUnitSphere; // jitter
-            droplet.GetComponent<Rigidbody>().velocity = dropletVelocity;
-        }
+            var surface = other.transform.parent.gameObject;
+            var canvas = surface.GetComponent<InkCanvas>();
 
-        Destroy(this.gameObject);
-    }
-
-    void Stamp(GameObject gameObject, Vector3 location)
-    {
-        var canvas = gameObject.GetComponent<InkCanvas>();
-
-        if (canvas != null)
-        {
             float ballSize = BallSize;
 
             // Assuming that the object we hit is the standard Unity Plane
-            float wallSizeX = 10 * gameObject.transform.localScale.x;
-            float wallSizeZ = 10 * gameObject.transform.localScale.z;
+            float wallSizeX = 10 * surface.transform.localScale.x;
+            float wallSizeZ = 10 * surface.transform.localScale.z;
 
             float scaleX = ballSize / wallSizeX;
             float scaleZ = ballSize / wallSizeZ;
 
+            Random.InitState((int)(this.transform.position.x * (2 ^ 20)));
             float jitteredSplashScale = Random.Range(splashMinScale, splashMaxScale);
             splashBrush.ScaleU = jitteredSplashScale * scaleX;
             splashBrush.ScaleV = jitteredSplashScale * scaleZ;
 
-            canvas.Paint(splashBrush, location);
+            Vector3 ballWorldPos = this.transform.position;
+            Vector3 ballSurfacePos = surface.transform.InverseTransformPoint(ballWorldPos);
+            Vector2 ballUV = new Vector2((-ballSurfacePos.x + 5) / 10, (-ballSurfacePos.z + 5) / 10);
+
+            if (ballUV.x + splashBrush.ScaleU / 2 < 0 ||
+                ballUV.x - splashBrush.ScaleU / 2 > 1 ||
+                ballUV.y + splashBrush.ScaleV / 2 < 0 ||
+                ballUV.y - splashBrush.ScaleV / 2 > 1)
+                return;
+
+            canvas.PaintUVDirect(splashBrush, ballUV);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PaintSurface"))
+        {
+            int droplets = Mathf.FloorToInt(BallSize * Random.Range(dropletMinAmount, dropletMaxAmount));
+            for (int i = 0; i < droplets; i++)
+            {
+                GameObject droplet = Instantiate(this.gameObject);
+                droplet.transform.localScale = this.transform.localScale * Random.Range(dropletMinSize, dropletMaxSize);
+                Vector3 dropletVelocity = this.GetComponent<Rigidbody>().velocity; // preserve
+                dropletVelocity += Random.Range(dropletMinReflection, dropletMaxReflection) * collision.contacts[0].normal; // reflect
+                dropletVelocity += dropletScatter * Random.insideUnitSphere; // jitter
+                droplet.GetComponent<Rigidbody>().velocity = dropletVelocity;
+            }
+
+            Destroy(this.gameObject);
         }
     }
 }
