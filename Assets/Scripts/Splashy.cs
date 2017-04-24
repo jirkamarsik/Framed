3771d1480775f49e0f465527d49e9e881c263646
Jirka Marsik <jiri.marsik89@gmail.com>
2017-04-24 12:38:36 +0200

    Balls roll around and splash more (slow)

--- Assets/Scripts/Splashy.cs
+++ Assets/Scripts/Splashy.cs
@@ -6,11 +6,32 @@ using Es.InkPainter;
 public class Splashy : MonoBehaviour {
 
     public Brush splashBrush;
-    public float splashScale = 3.0f;
+    public float splashScale = 7.0f;
+    public float stampInterval = 0.05f;
+    public float splashTime = 0.15f;
+    public float splashDecay = 0.9f;
+
+    private float lastStampTime = 0.0f;
 
     void OnCollisionEnter(Collision collision)
     {
-        var canvas = collision.gameObject.GetComponent<InkCanvas>();
+        Stamp(collision.gameObject, collision.contacts[0].point);
+        transform.localScale *= splashDecay;
+        StartCoroutine(DieLater(splashTime));
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Stamp(collision.gameObject, collision.contacts[0].point);
+        transform.localScale *= splashDecay;
+    }
+
+    void Stamp(GameObject surface, Vector3 location)
+    {
+        if (Time.time - lastStampTime < stampInterval)
+            return;
+
+        var canvas = surface.GetComponent<InkCanvas>();
 
         if (canvas != null)
         {
@@ -18,8 +39,8 @@ public class Splashy : MonoBehaviour {
             float ballSize = System.Math.Max(ballDimensions.x, System.Math.Max(ballDimensions.y, ballDimensions.z));
 
             // Assuming that the object we hit is the standard Unity Plane
-            float wallSizeX = 10 * collision.transform.localScale.x;
-            float wallSizeZ = 10 * collision.transform.localScale.z;
+            float wallSizeX = 10 * surface.transform.localScale.x;
+            float wallSizeZ = 10 * surface.transform.localScale.z;
 
             float scaleX = ballSize / wallSizeX;
             float scaleZ = ballSize / wallSizeZ;
@@ -27,9 +48,15 @@ public class Splashy : MonoBehaviour {
             splashBrush.ScaleU = splashScale * scaleX;
             splashBrush.ScaleV = splashScale * scaleZ;
 
-            canvas.Paint(splashBrush, collision.contacts[0].point);
-        }
+            canvas.Paint(splashBrush, location);
 
+            lastStampTime = Time.time;
+        }
+    }
+
+    IEnumerator DieLater(float deadline)
+    {
+        yield return new WaitForSeconds(deadline);
         Destroy(this.gameObject);
     }
 }
